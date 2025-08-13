8422a552bc70bc1faefe9474308c69c8772e7878
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-13 21:54:27 +0530

    add header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,3 +1,41 @@
+import Link from 'next/link';
+import { Edu_NSW_ACT_Cursive } from 'next/font/google';
+import { Button } from '../ui/button';
+import { useRouter } from 'next/navigation';
+
+const eduFont = Edu_NSW_ACT_Cursive({
+  subsets: ['latin'],
+});
+
 export default function Header() {
-  return <div>Header</div>;
+  const router = useRouter();
+
+  return (
+    <header className="m-4 mx-auto w-full max-w-6xl rounded-full bg-white p-3 text-black">
+      <div className="flex items-center justify-between">
+        <div>
+          <Link href="/">
+            <h1 className={`${eduFont.className} text-xl font-bold`}>
+              Draftly
+            </h1>
+          </Link>
+        </div>
+        <div className="flex items-center gap-6">
+          <Link href="/profile">
+            <span className="hover:text-neutral-600">Profile</span>
+          </Link>
+          <Link href="/post/create">
+            <span className="hover:text-neutral-600">Create Post</span>
+          </Link>
+          <Button
+            onClick={() => router.push('/')}
+            variant={'secondary'}
+            className="cursor-pointer"
+          >
+            Login
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 }
