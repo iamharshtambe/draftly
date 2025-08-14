98ec2cfdd0476a87559e80a2655d43b40963b766
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-14 18:48:59 +0530

    add login form

--- src/components/auth/auth-layout.tsx
+++ src/components/auth/auth-layout.tsx
@@ -6,11 +6,9 @@ import Register from './register';
 
 export default function AuthLayout() {
   return (
-    <div className="mx-auto my-10 flex max-w-6xl items-center justify-center">
-      <div className="mx-auto h-[500px] w-[450px] rounded-2xl bg-white p-4">
-        <h1 className="my-3 text-center text-2xl font-semibold text-black">
-          Welcome!
-        </h1>
+    <div className="my-20 flex items-center justify-center">
+      <div className="h-[500px] w-[450px] rounded-2xl border border-white p-4">
+        <h1 className="my-3 text-center text-2xl font-semibold">Welcome!</h1>
 
         <Tabs defaultValue="login" className="text-black">
           <TabsList className="w-full">
--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,3 +1,60 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form';
+import { Input } from '../ui/input';
+
+const loginSchema = z.object({
+  email: z.email('invlaid'),
+  password: z.string().min(6, 'Password must contain atleast 6 characters'),
+});
+
 export default function Login() {
-  return <div>Login</div>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form className="text-white">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="my-4">
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your email" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem className="my-4">
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Enter your email"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 }
