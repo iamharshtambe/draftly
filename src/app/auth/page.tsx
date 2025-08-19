95dd59171bec608e936786b6e2f1708b5991b11e
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-19 17:39:55 +0530

    initial commit

--- src/app/auth/page.tsx
+++
@@ -1,5 +0,0 @@
-import AuthLayout from '@/components/auth/auth-layout';
-
-export default function Authentication() {
-  return <AuthLayout />;
-}
--- src/app/post/[slug]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PostDetails() {
-  return <div>PostDetails</div>;
-}
